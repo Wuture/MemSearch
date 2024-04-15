49a1c8b3b4183a70e4065e2f95c0ba29805a4b11
Ka Ho Wu <kahowu@Kas-MacBook-Pro.local>
2024-04-16 06:53:53 +0800

    Removed all junk files

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# Background processes, one tracks the system status, the other tracks the screen status
-python3 system_tracker.py &
-# python3 schedule_analysis.py &
-python3 screen_tracker.py &
